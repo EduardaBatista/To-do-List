cc26ebed255426552806001835a76a24f8dd36cc
Eduarda Batista <110500574+EduardaBatista@users.noreply.github.com>
2023-03-21 16:46:02 -0300

    pimeiro commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
+import { useState } from "react";  
 import './App.css';
+import { AiOutlineCoffee } from 'react-icons/ai'
+import { BiCoffeeTogo} from 'react-icons/bi'
 
 function App() {
+  const [task, setTask] = useState()
+  const [list, setList] = useState([])
+
+  function handleAddTask(){
+    const itens= {'task': task,'data': new Date().toLocaleDateString()}
+    setList([itens, ...list])
+     clear()
+  }
+  function clear (){
+   setTask("")
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="adicionar">
+      <div className="caixaTexto">
+        <h1> Adicione tarefas:</h1>
+      <input value={task} onChange={ e => setTask(e.target.value)  } />
+      <button onClick={handleAddTask}><AiOutlineCoffee size={30} className="coffe"></AiOutlineCoffee></button>
+      </div>
+         <div className="tarefaas">
+        {list.map( t => (
+          <div className="lista">
+        <p> <BiCoffeeTogo  size={20} className='icon'/> {t.task} </p>
+        <span className="dataa">{t.data}</span>
+        </div>
+        ))}
+        </div>
+    </main>
   );
 }
 
